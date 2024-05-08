b42a48aa52f8ea506371611b303b35c69097fc8e
Rik Morskate <rik@foodticket.nl>
2024-05-08 15:29:44 +0200

    Removed unused import

--- src/Endpoints/SetOrderStatus.php
+++ src/Endpoints/SetOrderStatus.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Foodticket\Takeaway\Endpoints;
 
 use Carbon\Carbon;
-use Foodticket\Takeaway\Enums\OrderStatus;
 use Illuminate\Http\Client\RequestException;
 
 trait SetOrderStatus
